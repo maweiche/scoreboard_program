11e848abd5d69abff10b127d6c5e6cad906cffe0
init <beadatbeat@gmail.com>
2024-01-13 13:06:08 +0100

    Okay tests are runnning correctly
    Initializing Leaderboard seems to work

--- tests/scoreboard.ts
+++ tests/scoreboard.ts
@@ -11,10 +11,16 @@ describe('scoreboard', () => {
   
 
     const program = anchor.workspace.Scoreboard as Program<Scoreboard>;
+    
+    const testSigner = Keypair.generate();
+
+      // for debugging purposes
+    console.log("Seeds for PDA:", ["scoreboard", testSigner.publicKey.toBase58()]);
+
 
     it('Initializes the scoreboard', async () => {
        
-        const testSigner = Keypair.generate();
+    
 
         const res = await anchor.getProvider().connection.requestAirdrop(testSigner.publicKey, 1e9);
         await anchor.getProvider().connection.confirmTransaction(res, "confirmed");
@@ -39,8 +45,12 @@ describe('scoreboard', () => {
 
         await provider.connection.confirmTransaction(tx);
 
+        // for debugging purposes
+        console.log("testSigner's Public Key:", testSigner.publicKey.toBase58());
+
         const scoreboardAccount = await program.account.scoreboard.fetch(scoreboardPda);
-        assert.equal(scoreboardAccount.authority.toBase58(), provider.wallet.publicKey.toBase58());
+        assert.equal(scoreboardAccount.authority.toBase58(), testSigner.publicKey.toBase58());
+       
         // Assuming 'scores' is a valid field
         assert.deepEqual(scoreboardAccount.scores, []);
     });

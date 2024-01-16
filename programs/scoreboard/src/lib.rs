bb3d4e0f2d0d4fb2efd016a9839902f84c0883e1
Matt <maweiche@gmail.com>
2024-01-16 16:35:13 +0100

    added error msgs and signer checks to add score and reset scoreboard

--- programs/scoreboard/src/lib.rs
+++ programs/scoreboard/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 use std::mem::size_of;
 
-declare_id!("4uD5gHndD5G9ytPpMpQDJfahbekeYa28UgSA19XNAmVT");
+declare_id!("2qjPLYfvkTNB95mDgcnq4s8myTig2g1ya7HQfXwWJK5A");
 
-const MAX_SCORES: usize = 10; // Define the maximum number of scores
+const MAX_SCORES: usize = 20; // Define the maximum number of scores
 
 #[program]
 pub mod scoreboard {
@@ -22,7 +22,17 @@ pub mod scoreboard {
         let scoreboard = &mut ctx.accounts.scoreboard;
         let player = ctx.accounts.signer.key();
         let new_score = Score { player, score, timestamp };
-    
+        
+        // CHECK: The signer is the player who's score is being added
+        if ctx.accounts.signer.key() != new_score.player {
+            return Err(ErrorCode::WrongSigner.into());
+        }
+
+        // CHECK: The scoreboard is not full
+        if scoreboard.scores.len() == MAX_SCORES {
+            return Err(ErrorCode::ScoreboardFull.into());
+        }
+
         // Find the position to insert the new score
         let position = scoreboard.scores.iter()
                             .position(|x| x.score <= new_score.score)
@@ -34,9 +44,11 @@ pub mod scoreboard {
         Ok(())
     }
     
-
     // Function to reset scoreboard
     pub fn reset_scoreboard(ctx: Context<ResetScoreboardContext>) -> Result<()> {
+        if ctx.accounts.signer.key() != ctx.accounts.scoreboard.authority {
+            return Err(ErrorCode::Unauthorized.into());
+        }
         let scoreboard = &mut ctx.accounts.scoreboard;
         scoreboard.scores = Vec::new(); // Reset the scores vector
         Ok(())
@@ -62,7 +74,7 @@ pub struct InitializeScoreboard<'info> {
 pub struct AddScoreContext<'info> {
     #[account(mut)]
     pub scoreboard: Account<'info, Scoreboard>,
-    /// CHECK: The signer is the player who's score is being added
+    /// CHECK: This is not dangerous because the signer is checked in the program
     #[account(signer)]
     pub signer: AccountInfo<'info>,
 }
@@ -71,6 +83,9 @@ pub struct AddScoreContext<'info> {
 pub struct ResetScoreboardContext<'info> {
     #[account(mut)]
     pub scoreboard: Account<'info, Scoreboard>,
+    /// CHECK: This is not dangerous because the signer is checked in the program
+    #[account(signer)]
+    pub signer: AccountInfo<'info>,
 }
 
 #[account]
@@ -85,3 +100,18 @@ pub struct Score {
     pub score: u64,
     pub timestamp: i64,
 }
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+
+    #[msg("Signer does not match player.")]
+    WrongSigner,
+
+    #[msg("Scoreboard is full.")]
+    ScoreboardFull,
+}
+
+
+
